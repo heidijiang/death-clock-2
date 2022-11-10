3994c9ba384aa5eb0e02ba5598ae6ba1fbdba3d5
Heidi Jiang <jiang.heidi@gmail.com>
2022-11-10 10:34:08 -0500

    fixes

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,4 @@ WORKDIR /app
 RUN pip3 install -r requirements.txt
 
 COPY . /app
-ENTRYPOINT [ "python3" ]
-CMD [ "app.py" ]
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0", "--port=5000"]
--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-app="docker3.test"
+app="docker.test"
 docker build --platform linux/amd64 -t ${app} .
-docker run --platform linux/amd64 -d -p 56733:80 \
-  --name=${app} \
-  -v $PWD:/app ${app}
+docker run --platform linux/amd64 -d -p 56733:80 ${app}
