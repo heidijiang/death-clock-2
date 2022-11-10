b103093fde17ac9cbae5cdeb4e031410027a8d90
Heidi Jiang <jiang.heidi@gmail.com>
2022-11-10 10:47:29 -0500

    wrong port

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 app="docker.test"
 docker build --platform linux/amd64 -t ${app} .
-docker run --platform linux/amd64 -d -p 56733:80 ${app}
+docker run --platform linux/amd64 -d -p 5000:5000 ${app}
