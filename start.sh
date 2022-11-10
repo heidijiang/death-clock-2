d6cfcc25e58c17a260dedc6af40123273a04fb82
Heidi Jiang <jiang.heidi@gmail.com>
2022-11-09 21:44:58 -0500

    force linux platform

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app="docker.test"
-docker build -t ${app} .
-docker run -d -p 56733:80 \
+docker build --platform linux/amd64 -t ${app} .
+docker run --platform linux/amd64 -d -p 56733:80 \
   --name=${app} \
   -v $PWD:/app ${app}
